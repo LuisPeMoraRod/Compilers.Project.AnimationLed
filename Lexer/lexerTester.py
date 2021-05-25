c8221caa9019ef66339c2641ac502a02d8ee6ca4
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2021-05-25 12:08:32 -0600

    Add main keywords, expressions and identifier analysis
    Version of the lexer where it can recognize numbers, keywords (for, if, procedure, true and false), identifiers (of functions or variables) and other symbols like punctuation marks and brackets

--- Lexer/lexerTester.py
+++ Lexer/lexerTester.py
@@ -1,4 +1,13 @@
+from  lexer import Lexer
+from lexer import TokenType
+
 def main():
-    print("TEST")
+    input = "If+-123 foo*For/ true false"
+    lexer = Lexer(input)
+
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
 
 main()
